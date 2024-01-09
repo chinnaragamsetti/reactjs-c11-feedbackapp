0e5c4ead579c8584dc923735a07f32dc58822ed9
chinnaragamsetti <ragamsettirajkumar@gmail.com>
2024-01-09 09:53:01 +0000

    allchanges3

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -13,56 +13,31 @@ class Feedback extends Component {
     const {isClicked} = this.state
     const {resources} = this.props
     const {emojis, loveEmojiUrl} = resources
+    const {id, name, imageUrl} = emojis
     if (isClicked === true) {
       return (
         <div className="feedbackcontainer">
           <h1 className="heading">
             How satisfied are you with our customer support performance
           </h1>
-          <div className="emojiscontainer">
-            <div className="emojicontainer">
-              <button type="button" id="label" onClick={this.onChange}>
-<<<<<<< HEAD
-                <img src={emojis[0].imageUrl} alt="name" className="image" />
-=======
-                <img src={emojis[0].imageUrl} alt="Sad" className="image" />
->>>>>>> d3580f69b19542392f63b7d3b73c706098961386
-              </button>
-              <label htmlFor="labell" className="emojipara">
-                {emojis[0].name}
-              </label>
-            </div>
-            <div className="emojicontainer">
-              <button type="button" id="label" onClick={this.onChange}>
-<<<<<<< HEAD
-                <img src={emojis[1].imageUrl} alt="name" className="image" />
-=======
-                <img src={emojis[1].imageUrl} alt="None" className="image" />
->>>>>>> d3580f69b19542392f63b7d3b73c706098961386
-              </button>
-              <label htmlFor="labell" className="emojipara">
-                {emojis[1].name}
-              </label>
-            </div>
-            <div className="emojicontainer">
-              <button type="button" id="label" onClick={this.onChange}>
-<<<<<<< HEAD
-                <img src={emojis[2].imageUrl} alt="name" className="image" />
-=======
-                <img src={emojis[2].imageUrl} alt="Happy" className="image" />
->>>>>>> d3580f69b19542392f63b7d3b73c706098961386
-              </button>
-              <label htmlFor="labell" className="emojipara">
-                {emojis[2].name}
-              </label>
-            </div>
-          </div>
+          <ul className="emojiscontainer">
+            {resources.map(each => (
+              <li key={each.id} className="emojicontainer">
+                <button type="button" id="label" onClick={this.onChange}>
+                  <img src={each.imageUrl} alt="name" className="image" />
+                </button>
+                <label htmlFor="labell" className="emojipara">
+                  {each.name}
+                </label>
+              </li>
+            ))}
+          </ul>
         </div>
       )
     }
     return (
       <div className="thankyoucontainer">
-        <img src={loveEmojiUrl} alt="Love emoji" className="love" />
+        <img src={loveEmojiUrl} alt="love emoji" className="love" />
         <h1 className="para1">Thank You!</h1>
         <p className="para2">
           We will use your feedback to improve our customer support perfomance
